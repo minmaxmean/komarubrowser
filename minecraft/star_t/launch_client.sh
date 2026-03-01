663339bd8f9c94e2687d1bf135ed4f13a190d8d8
Alibek Manabayev <alibek.manabayev@gmail.com>
2026-03-01 16:44:54 +0000

    run star_t client using bazel

---
+++ minecraft/star_t/launch_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Environment variables
+STAR_T_CLIENT_DIR="${STAR_T_CLIENT_DIR:?STAR_T_CLIENT_DIR must be set}"
+
+# The mod jar is passed as the first argument by Bazel
+MOD_JAR="$1"
+
+echo "Copying mod to client directory..."
+mkdir -p "$STAR_T_CLIENT_DIR/mods"
+cp "$MOD_JAR" "$STAR_T_CLIENT_DIR/mods/"
+
+echo "Launching PrismLauncher..."
+prismlauncher --launch 'KomaruBrowser' -s 'localhost:26767' 2>/dev/null
