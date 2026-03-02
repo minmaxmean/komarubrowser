ffec38f2e24f37d41e68d73aa16c51cb07bc1e59
Alibek Manabayev <alibek.manabayev@gmail.com>
2026-03-02 03:08:30 +0000

    copy assets

--- ts/web/vite.config.ts
+++ ts/web/vite.config.ts
@@ -1,5 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import * as path from 'path';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+const __ALLOW_SERVE = path.join(__dirname, '/../../node_modules');
+// console.log('===DEBUG===', { cwd: process.cwd(), env: process.env, ALLOW_SERVE:__ALLOW_SERVE });
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		fs: {
+			allow: [__ALLOW_SERVE],
+		}
+	}
+});
