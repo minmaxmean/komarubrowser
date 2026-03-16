8e76840ddfc022c7d96181e2b347c4377bc5d96b
Alibek Manabayev <alibek.manabayev@gmail.com>
2026-03-16 02:26:25 +0000

    calcDirectedEdges

--- ts/web/src/lib/calc/edges.test.ts
+++ ts/web/src/lib/calc/edges.test.ts
@@ -1,8 +1,14 @@
 import { assert, describe, test } from 'vitest';
 import type { Recipe } from '@komarubrowser/common/db/recipe';
 import type { CustomsMap } from '$lib/appstate/customs';
-import { type CalculatedEdge, calcEdges } from './edges.ts';
-import { mapFromObject, shortRecipe } from './testutis.ts';
+import {
+  type CalculatedEdge,
+  type DirectedEdgeList,
+  type DirectedEdges,
+  calcDirectedEdges,
+  calcEdges,
+} from './edges.ts';
+import { mapFromObject, shortCustom, shortRecipe } from './testutis.ts';
 
 describe('calcEdges', () => {
   test('H2SO4 - bamboo', () => {
@@ -18,5 +24,60 @@ describe('calcEdges', () => {
     ];
     const got = calcEdges(recipes, customs);
     assert.deepEqual(got, want);
+  });
+});
+
+type ShortDirectedEdgeListArg = { poopsTo?: string[]; eatsFrom?: string[] };
+const shortDirectedEdgeList = ({
+  poopsTo,
+  eatsFrom,
+}: ShortDirectedEdgeListArg): DirectedEdgeList => ({
+  poopsTo: new Set(poopsTo),
+  eatsFrom: new Set(eatsFrom),
+});
+
+describe('calcDirectedEdges', () => {
+  describe('H2SO4 - bamboo', () => {
+    const recipes: Recipe[] = [
+      shortRecipe('SO2', { S: 1, O: 2000 }, { SO2: 1000 }, 3 * 20, 'ULV'),
+      shortRecipe('SO3', { SO2: 1000, O: 1000 }, { SO3: 1000 }, 10 * 20, 'ULV'),
+      shortRecipe('H2SO4', { SO3: 1000, H2O: 1000 }, { H2SO4: 1000 }, 8 * 20, 'ULV'),
+    ];
+    test('SO2 = 1', () => {
+      const customs: CustomsMap = mapFromObject({
+        SO2: shortCustom({ cnt: 1 }),
+      });
+      const want: DirectedEdges = mapFromObject({
+        SO2: shortDirectedEdgeList({ poopsTo: ['SO3'] }),
+        SO3: shortDirectedEdgeList({ poopsTo: ['H2SO4'] }),
+        H2SO4: shortDirectedEdgeList({}),
+      });
+      const got = calcDirectedEdges(recipes, customs);
+      assert.deepEqual(got, want);
+    });
+    test('SO3 = 1', () => {
+      const customs: CustomsMap = mapFromObject({
+        SO3: shortCustom({ cnt: 1 }),
+      });
+      const want: DirectedEdges = mapFromObject({
+        SO2: shortDirectedEdgeList({}),
+        SO3: shortDirectedEdgeList({ eatsFrom: ['SO2'], poopsTo: ['H2SO4'] }),
+        H2SO4: shortDirectedEdgeList({}),
+      });
+      const got = calcDirectedEdges(recipes, customs);
+      assert.deepEqual(got, want);
+    });
+    test('H2SO4 = 1', () => {
+      const customs: CustomsMap = mapFromObject({
+        H2SO4: shortCustom({ cnt: 1 }),
+      });
+      const want: DirectedEdges = mapFromObject({
+        SO2: shortDirectedEdgeList({}),
+        SO3: shortDirectedEdgeList({ eatsFrom: ['SO2'] }),
+        H2SO4: shortDirectedEdgeList({ eatsFrom: ['SO3'] }),
+      });
+      const got = calcDirectedEdges(recipes, customs);
+      assert.deepEqual(got, want);
+    });
   });
 });
--- ts/web/src/lib/calc/edges.ts
+++ ts/web/src/lib/calc/edges.ts
@@ -28,3 +28,47 @@ export const calcEdges = (recipes: Recipe[], customs: CustomsMap): CalculatedEdg
       );
     });
   });
+
+export type DirectedEdgeList = { poopsTo: Set<string>; eatsFrom: Set<String> };
+export type DirectedEdges = Map<string, DirectedEdgeList>;
+
+// Do BFS to direct edges from anchors
+export const calcDirectedEdges = (recipes: Recipe[], customs: CustomsMap): DirectedEdges => {
+  const undirectedEdges = calcEdges(recipes, customs);
+  const edges: DirectedEdges = new Map();
+  // Start Queue with anchors
+  const queue: string[] = customs
+    .entries()
+    .filter(([_, custom]) => !custom.isAuto && custom.cnt)
+    .map(([nodeId]) => nodeId)
+    .toArray();
+  const queuedUp = new Set<string>(queue);
+  while (queue.length > 0) {
+    const me = queue.splice(0, 1)[0];
+    const myEdges: DirectedEdgeList = edges.get(me) ?? { poopsTo: new Set(), eatsFrom: new Set() };
+    for (const edge of undirectedEdges) {
+      if (edge.source === me) {
+        // me = producer, target = consumer
+        const alreadyExists = edges.get(edge.target)?.eatsFrom.has(me) || false;
+        if (alreadyExists) continue;
+        if (!queuedUp.has(edge.target)) {
+          queuedUp.add(edge.target);
+          queue.push(edge.target);
+        }
+        myEdges.poopsTo.add(edge.target);
+      } else if (edge.target === me) {
+        // me = consumer, source = producer
+        const alreadyExists = edges.get(edge.source)?.poopsTo.has(me) || false;
+        if (alreadyExists) continue;
+        if (!queuedUp.has(edge.source)) {
+          queuedUp.add(edge.source);
+          queue.push(edge.source);
+        }
+        myEdges.eatsFrom.add(edge.source);
+      }
+    }
+    edges.set(me, myEdges);
+  }
+
+  return edges;
+};
